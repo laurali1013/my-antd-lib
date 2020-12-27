6ba24f6f6585c9e25fac6621dae4f7c714163ece
laurali1013 <785624889@qq.com>
2020-12-27 20:11:01 +0800

    自定义hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import './App.css';
 
 import MouseTracker from './learn-react-hook/03MouseTracker';
 import LikeButton from './learn-react-hook/02LikeButton';
+import useMousePosition from './learn-react-hook/04useMousePosition';
 
 function App() {
   const [show, setShow] = useState(true);
+  const positions = useMousePosition();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           <button
             onClick={() => {
@@ -21,6 +24,9 @@ function App() {
           </button>
         </p>
         {show && <MouseTracker />}
+        <p>
+          X:{positions.x},Y:{positions.y}
+        </p>
         <LikeButton />
         <a
           className="App-link"

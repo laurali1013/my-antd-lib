7d9b22714a5d869a7007fb118c3916f9d881d20b
laurali1013 <785624889@qq.com>
2020-12-27 17:40:02 +0800

    add document.title,useEffect

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
     - create-react-app下载到临时目录
     - 调用项目内部安装的模块
 # 3-5：useState Hook
- - setState是替换不是合并，所以每次setState都要把该state的内容全部赋值一遍
+  - setState是替换不是合并，所以每次setState都要把该state的内容全部赋值一遍
     
+# 3-6:useEffect hook
+  - 
--- src/learn-react-hook/02LikeButton.tsx
+++ src/learn-react-hook/02LikeButton.tsx
@@ -1,9 +1,13 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 
 //setState是替换不是合并，所以每次setState都要把该state的内容全部赋值一遍
 const LikeButton: FC = () => {
   const [like, setLike] = useState(0);
   const [on, setOn] = useState(true);
+  //不需要清除的副作用
+  useEffect(() => {
+    document.title = `点击了${like}次`;
+  });
   return (
     <>
       <button
